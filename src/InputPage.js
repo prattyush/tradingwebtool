90e125d0c82a8b38c909b140c9becb00877be896
Prattyush Banerjee <prattyush.banerjee1501@gmail.com>
2025-08-30 22:44:43 +0530

    Fixing Default Values

--- src/InputPage.js
+++ src/InputPage.js
@@ -8,7 +8,7 @@ const InputPage = () => {
     const year = today.getFullYear();
     const month = String(today.getMonth() + 1).padStart(2, '0'); // Add 1 as months are 0-indexed
     const day = String(today.getDate()).padStart(2, '0');
-    const formattedDate = `${year}-${month}-${day}`;
+    const formattedDate = `${day}-${month}-${year}`;
 
     const [tradeDate, setTradeDate] = useState(formattedDate)
     const [ceStrikeprice, setCEStrikePrice] = useState(0)
