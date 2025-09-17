b02dd5775f5b896efb248d2a689d5cac8719f239
Prattyush Banerjee <prattyush.banerjee1501@gmail.com>
2025-09-18 02:29:43 +0530

    Strategy

--- src/TradeReplayerOptions.js
+++ src/TradeReplayerOptions.js
@@ -2,7 +2,13 @@ import {useState, useCallback, useRef, useEffect, use} from "react";
 import {useLocation} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import {CandlestickSeries, createChart, LineSeries, LineStyle} from "lightweight-charts";
-import {strategyoptions, optionstype, orderTypeOptions, optionvaluerangeoptions} from "./StrategyVariables";
+import {
+    strategyoptions,
+    optionstype,
+    orderTypeOptions,
+    optionvaluerangeoptions,
+    tradestrategyoptions
+} from "./StrategyVariables";
 import {priceIntervalRangeMap} from "./StrategyVariables";
 
 const TradeReplayerOptions = () => {
@@ -11,6 +17,7 @@ const TradeReplayerOptions = () => {
     const navigate = useNavigate();
     const ipAddress = location.state['ipAddress'];
     const [tradeDate, setTradeDate] = useState("")
+    const [tradeStrategy, setTradeStrategy] = useState("none")
     const timeBarCount = useRef(1);
     const currDayTotalBars = 120;
     const currentTotalPEQuantity = useRef(0);
@@ -417,6 +424,7 @@ const TradeReplayerOptions = () => {
         const dataToSend = {
             'tradedate': tradeDate,
             'tradesList': tradeRecorder,
+            'strategy': tradeStrategy,
             'type':'tradesubmit'
         };
         const jsonString = JSON.stringify(dataToSend);
@@ -595,6 +603,14 @@ const TradeReplayerOptions = () => {
 
                     <button type="button" onClick={handleBuyOptions} title="buyoptions" style={{float:"left", marginTop:"1%", marginRight:'1%', marginLeft:'1%'}}>Buy</button>
                     <button type="button" onClick={handleSellOptions} title="selloptions" style={{float:"left", marginTop:"1%", marginRight:'1%', marginLeft:'1%'}}>Sell</button>
+                    <label style={{clear:"both", float:"left", marginTop:'1%'}}>Strategy:: </label>
+                    <select style={{float:"left", marginTop:'1%', fontSize:'.75rem'}} name="tradeStrategy" id="tradeStrategy" defaultValue="none" onChange={(e) => setTradeStrategy(e.target.value)}>
+                        {tradestrategyoptions.map((option) => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div style={{clear:"both", float:"left", width:"80%", marginTop:'3%'}}>
                     <button type="button" onClick={handleSubmitTradeData} title="submitTradeData" style={{float:"left", marginTop:"1%", marginRight:'1%', marginLeft:'1%'}}>Submit Trade Data</button>
